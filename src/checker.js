76df13c4833ad7e44f219c38753884738cc06ec5
Himanshu Sukhwani <himanshu_sukhwani@gmail.com>
2025-12-10 23:08:17 +0530

    fix: batch OSV API requests to avoid 400 error

--- src/checker.js
+++ src/checker.js
@@ -6,7 +6,6 @@ const OSV_API_URL = 'https://api.osv.dev/v1/querybatch';
 export async function checkVulnerabilities(dependencies) {
     const queries = Object.entries(dependencies).map(([name, version]) => {
         const cleanVersion = version.replace(/[^0-9.]/g, '');
-
         return {
             package: {
                 name: name,
@@ -16,9 +15,19 @@ export async function checkVulnerabilities(dependencies) {
         };
     });
 
+    const BATCH_SIZE = 500;
+    const chunks = [];
+    for (let i = 0; i < queries.length; i += BATCH_SIZE) {
+        chunks.push(queries.slice(i, i + BATCH_SIZE));
+    }
+
     try {
-        const response = await axios.post(OSV_API_URL, { queries });
-        return response.data.results;
+        const promises = chunks.map(chunk => axios.post(OSV_API_URL, { queries: chunk }));
+        const responses = await Promise.all(promises);
+
+        // Merge results from all batches
+        const allResults = responses.flatMap(response => response.data.results);
+        return allResults;
     } catch (error) {
         if (error.response) {
             console.error(chalk.red(`OSV API Error: ${error.response.status} ${error.response.statusText}`));
